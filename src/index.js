3f7ef1cbb9ed75b21cdd99cecd0164516c3ba117
Aaron <fong_lun@live.com>
2022-12-19 15:56:27 -0500

    21

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ require('file-loader?name=[name].[ext]!./index.html');
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import  App  from './App';
-import './App.scss';
+import './App.scss'
+
 
 // const appElement = document.getElementById('app');
 
